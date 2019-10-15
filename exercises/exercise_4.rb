38aa24645fba1686e5b611e673a7377db1dfeffc
ChrisnNg <cristopherng@hotmail.com>
2019-10-15 20:03:39 +0000

    exercise 3/4 complete

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,39 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+surrey = Store.create do |s|
+  s.name = "Surrey"
+  s.annual_revenue = 224000
+  s.mens_apparel = false
+  s.womens_apparel = true
+end
+
+
+whistler = Store.create do |s|
+  s.name = "Whistler"
+  s.annual_revenue = 1900000
+  s.mens_apparel = true
+  s.womens_apparel = false
+end
+
+
+yaletown = Store.create do |s|
+  s.name = "Yaletown"
+  s.annual_revenue = 430000
+  s.mens_apparel = true
+  s.womens_apparel = true
+end
+
+@mens_stores = Store.where(mens_apparel: true)
+
+puts '-----output-----'
+
+@mens_stores.each do |store|
+  print store.name
+  print ' '
+  print store.annual_revenue
+  puts
+end
+@womens_stores = Store.where(womens_apparel: true)
+@womens_stores = @womens_stores.where("annual_revenue > ?", 1000000 )
+puts @womens_stores.count
